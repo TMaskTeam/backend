5d45d3ab3c394894c0d8f643ed1dbf12afed436a
Malov Tim <t.malov2014@gmail.com>
2026-05-15 23:18:33 +0300

    TASK-16: make full repo

--- src/internal/repository/abstract/business_owner.go
+++ src/internal/repository/abstract/business_owner.go
@@ -9,11 +9,8 @@ type IBusinessOwnerRepository interface {
 	Upsert(conn abstract.IDBConnection, owner *domain.BusinessOwner) error
 	Delete(conn abstract.IDBConnection, ownerID int) error
 
+	GetPasswordHashById(conn abstract.IDBConnection, ownerID int) (string, error)
 	GetByINN(conn abstract.IDBConnection, inn string) (*domain.BusinessOwner, error)
 	GetByPhoneNumber(conn abstract.IDBConnection, phoneNumber string) (*domain.BusinessOwner, error)
 	GetByEmail(conn abstract.IDBConnection, email string) (*domain.BusinessOwner, error)
-
-	ExistsByINN(conn abstract.IDBConnection, inn string) (bool, error)
-	ExistsByPhoneNumber(conn abstract.IDBConnection, phoneNumber string) (bool, error)
-	ExistsByEmail(conn abstract.IDBConnection, email string) (bool, error)
 }
--- src/internal/repository/impl/business_owner.go
+++ src/internal/repository/impl/business_owner.go
@@ -36,3 +36,67 @@ func (bo *BusinessOwnerRepository) Upsert(conn abstract.IDBConnection, owner *do
 	}).
 		Create(ownerDAO).Error
 }
+
+func (bo *BusinessOwnerRepository) Delete(conn abstract.IDBConnection, ownerID int) error {
+	db := conn.Get().(*gorm.DB)
+	return db.Where("owner_id = ?", ownerID).Delete(&model.BusinessOwner{}).Error
+}
+
+func (bo *BusinessOwnerRepository) GetPasswordHashById(conn abstract.IDBConnection, ownerID int) (string, error) {
+	db := conn.Get().(*gorm.DB)
+
+	var hashedPassword string
+	err := db.Model(&model.BusinessOwner{}).
+		Where("owner_id = ?", ownerID).
+		Select("password_hash").Error
+	if err != nil {
+		return "", err
+	}
+
+	return hashedPassword, err
+}
+
+func (bo *BusinessOwnerRepository) GetByINN(conn abstract.IDBConnection, inn string) (*domain.BusinessOwner, error) {
+	db := conn.Get().(*gorm.DB)
+
+	var ownerDAO model.BusinessOwner
+	err := db.Where("inn = ?", inn).First(&ownerDAO).Error
+	if err != nil {
+		if err == gorm.ErrRecordNotFound {
+			return nil, nil
+		}
+		return nil, err
+	}
+
+	return ownerDAO.ToDomain()
+}
+
+func (bo *BusinessOwnerRepository) GetByPhoneNumber(conn abstract.IDBConnection, phoneNumber string) (*domain.BusinessOwner, error) {
+	db := conn.Get().(*gorm.DB)
+
+	var ownerDAO model.BusinessOwner
+	err := db.Where("phone_number = ?", phoneNumber).First(&ownerDAO).Error
+	if err != nil {
+		if err == gorm.ErrRecordNotFound {
+			return nil, nil
+		}
+		return nil, err
+	}
+
+	return ownerDAO.ToDomain()
+}
+
+func (bo *BusinessOwnerRepository) GetByEmail(conn abstract.IDBConnection, email string) (*domain.BusinessOwner, error) {
+	db := conn.Get().(*gorm.DB)
+
+	var ownerDAO model.BusinessOwner
+	err := db.Where("email = ?", email).First(&ownerDAO).Error
+	if err != nil {
+		if err == gorm.ErrRecordNotFound {
+			return nil, nil
+		}
+		return nil, err
+	}
+
+	return ownerDAO.ToDomain()
+}

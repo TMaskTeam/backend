abc63e5cf19768577295c486989bd520a1c46782
Petr Baskakov <mart040316@gmail.com>
2026-05-16 00:43:51 +0300

    Little change migrations

--- migration/20260515071000_create_business_owner_table.sql
+++ migration/20260515071000_create_business_owner_table.sql
@@ -9,10 +9,11 @@ CREATE TABLE IF NOT EXISTS mask.business_owner (
     inn             TEXT        UNIQUE          NOT NULL,
     phone_number    TEXT        UNIQUE          NOT NULL,
     email           TEXT        UNIQUE          NOT NULL,
-    birthday         DATE                        NOT NULL,   
+    birthday        DATE                        NOT NULL,   
     created_at      TIMESTAMP,
     updated_at      TIMESTAMP   
 );   
 
 -- +goose Down
 DROP TABLE IF EXISTS    mask.business_owner;
+
--- migration/20260515132902_business_table.sql
+++ migration/20260515132902_business_table.sql
@@ -2,13 +2,12 @@
 CREATE SCHEMA IF NOT EXISTS mask;
 
 CREATE TABLE IF NOT EXISTS mask.business (
-    business_id  SERIAL      NOT NULL,
-    owner_id     INTEGER     NOT NULL,
-    name         TEXT        NOT NULL,
-    address      TEXT        NOT NULL,
-    created_at   TIMESTAMP,
-    updated_at   TIMESTAMP,
-    PRIMARY KEY (business_id)
+    business_id     SERIAL          PRIMARY KEY,
+    owner_id        INTEGER         NOT NULL,
+    name            TEXT            NOT NULL,
+    address         TEXT            NOT NULL,
+    created_at      TIMESTAMP,
+    updated_at      TIMESTAMP,
 );
 
 
@@ -22,3 +21,6 @@ ON DELETE CASCADE;
 -- +goose Down
 ALTER TABLE mask.business DROP CONSTRAINT IF EXISTS fk_business_owner;
 DROP TABLE IF EXISTS    mask.business;
+
+
+

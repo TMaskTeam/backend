ff98d8493b554e28d0c0e3a38692a0d94044e7f3
Malov Tim <t.malov2014@gmail.com>
2026-05-15 18:07:25 +0300

    TASK-16: add upsert

--- src/internal/repository/impl/business_owner.go
+++ src/internal/repository/impl/business_owner.go
@@ -3,8 +3,10 @@ package impl
 import (
 	"backend/src/internal/db/abstract"
 	"backend/src/internal/domain"
+	"backend/src/internal/model"
 
 	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
 )
 
 type BusinessOwnerRepository struct{}
@@ -16,4 +18,21 @@ func NewBusinessOwnerRepository() BusinessOwnerRepository {
 func (bo *BusinessOwnerRepository) Upsert(conn abstract.IDBConnection, owner *domain.BusinessOwner) error {
 	db := conn.Get().(*gorm.DB)
 
+	ownerDAO := &model.BusinessOwner{}
+	ownerDAO, err := ownerDAO.ToModel(owner)
+	if err != nil {
+		return err
+	}
+
+	return db.Clauses(clause.OnConflict{
+		Columns: []clause.Column{
+			{Name: "code"},
+			{Name: "owner_id"},
+			{Name: "phone_number"},
+			{Name: "inn"},
+			{Name: "email"},
+		},
+		UpdateAll: true,
+	}).
+		Create(ownerDAO).Error
 }

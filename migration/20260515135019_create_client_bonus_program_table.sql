4cdaf72ecc89836a672fb696906f37cee58dd480
k3rnel1x <k3rnel1x@MacBook-Air-k3rnel1x.local>
2026-05-15 18:16:45 +0300

    TASK-15: token_count INT, add down for fk_program_id and fk_client_id

--- migration/20260515135019_create_client_bonus_program_table.sql
+++ migration/20260515135019_create_client_bonus_program_table.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS mask.client_bonus_program (
     client_bonus_program  SERIAL      PRIMARY KEY,
     program_id            INT,
     client_id             INT,
-    tokens_count          TEXT,
+    tokens_count          INT,
     created_at            TIMESTAMP,
     updated_at            TIMESTAMP   
 );  
@@ -16,11 +16,13 @@ FOREIGN KEY (program_id)
 REFERENCES mask.bonus_program(program_id)
 ON DELETE CASCADE;
 
-ALTER TABLE mask.client
+ALTER TABLE mask.client_bonus_program
 ADD CONSTRAINT fk_client_id
 FOREIGN KEY (client_id)
 REFERENCES mask.client(client_id)
 ON DELETE CASCADE;
 
 -- +goose Down
+ALTER TABLE mask.client_bonus_program DROP CONSTRAINT IF EXISTS fk_program_id;
+ALTER TABLE mask.client_bonus_program DROP CONSTRAINT IF EXISTS fk_client_id;
 DROP TABLE IF EXISTS    mask.client_bonus_program;
